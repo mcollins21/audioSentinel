4c852fdf4db7bae46dd29c104760bd0b7f7253e2
mcollins21 <mcollins21@luc.edu>
2024-01-17 23:15:43 -0500

    centered buttons and output window

--- audioSentinel.py
+++ audioSentinel.py
@@ -10,7 +10,7 @@
 import sys
 from pyo import *
 from src import midiUtils
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QHBoxLayout
 from PyQt5.QtCore import QObject, pyqtSignal
 
 class OutputRedirector(QObject):
@@ -36,22 +36,42 @@ def driverOutputSearch():
     server = Server().boot()
     midiUtils.deviceOutputs()
 
+def driverDeviceCount():
+    server = Server().boot()
+    midiUtils.deviceCount()
+
 window = QWidget()
 window.setWindowTitle("Audio Device Scanner")
 
+window.setFixedSize(300, 300)
+
+layout = QHBoxLayout()
+window.setLayout(layout)  # Set the layout for the window
+"""
+# Add buttons to the layout
+layout.addWidget(scan_button)
+layout.addWidget(scan_button, stretch=1)  # Add spacing between buttons
+layout.addWidget(scan_button)
+"""
+
+# device count button
+scan_button = QPushButton("Device Count", window)
+scan_button.clicked.connect(driverDeviceCount)
+scan_button.move(100, 20)
+
 # input scanner button
-scan_button = QPushButton("Scan Input Devices", window)
+scan_button = QPushButton("Input", window)
 scan_button.clicked.connect(driverInputSearch)
-scan_button.move(100, 50)
+scan_button.move(100, 60)
 
 # output scanner button
-scan_button = QPushButton("Scan Output Devices", window)
+scan_button = QPushButton("Output", window)
 scan_button.clicked.connect(driverOutputSearch)
-scan_button.move(100, 0)
+scan_button.move(100, 100)
 
 # output window
 output_text = QTextEdit(window)
-output_text.move(50, 100)  # Adjust position as needed
+output_text.setGeometry(50, 140, 200, 150)  # Adjusted size and position
 output_text.setReadOnly(True)  # Make it read-only
 
 # Connect the OutputRedirector signal to update the QTextEdit
